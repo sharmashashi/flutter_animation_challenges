4cb0b318c501bdee79581ed3dd4a453fba460866
Shashi Sharma <sharmashashi940@gmail.com>
2022-08-12 21:55:55 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation_challenges/pages/homepage.dart';
+import 'package:flutter_animation_challenges/pages/weather/weather_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(
+    child: MaterialApp(
+      home: WeatherPage(),
+    ),
+  ));
+}
---
+++ lib/provider/weather_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final weatherProvider = StateNotifierProvider(((ref) => WeatherProvider(ref)));
+
+class WeatherProvider extends StateNotifier {
+  final Ref ref;
+  WeatherProvider(this.ref) : super(null);
+  final backgroundColor1 = const Color(0xff444c4f);
+  final backgroundColor2 = const Color(0xff1f2527);
+  final foregroundColor1 = const Color(0xff7e8588);
+  final foregroundColor2 = const Color(0xffe0e2e2);
+  final foregroundColor3 = const Color(0xffbfc1c1);
+
+  final currentTemperature = "-79\u00b0";
+  final dateTitle = "TODAY: ";
+  final dateValue =
+      "${DateTime.now().year}.${DateTime.now().month}.${DateTime.now().day}";
+  final minimumTemperature = "-98\u00b0";
+  final maximumTemperature = "-2\u00b0";
+  final location = "Kathmandu, Nepal";
+  final temperatureList = [
+    "-20\u00b0",
+    "-50\u00b0",
+    "-16\u00b0",
+    "-37\u00b0",
+    "-10\u00b0",
+  ];
+  final weatherDescription = "Snow turn to Cloudy";
+  final appBarText = "Weather";
+}
